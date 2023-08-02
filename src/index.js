5b8ea6a0b755c73278de65d4a6f43b16b74e0df5
Billy <billychorey@gmail.com>
2023-08-02 17:10:45 -0400

    tasklist mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+  });
+
+  const submitBtn = document.getElementById('submit-btn');
+  const taskList = document.getElementById('tasks');
+  submitBtn.addEventListener('click', handleSubmit);
+  
+  function handleSubmit() {
+    const submitText = document.getElementById('new-task-description').value;
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.textContent = 'x';
+    li.textContent = submitText;
+    li.appendChild(btn);
+
+    taskList.appendChild(li);
+
+    // Add event listener directly to the 'x' button
+    btn.addEventListener('click', function() {
+      const parentDiv = this.parentNode;
+      parentDiv.remove();
+    });
+  }
 });
